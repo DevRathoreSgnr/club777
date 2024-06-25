533e279f42d78ddc56b5cb7a2880b14bded1ecc7
Dev Rathore <108182126+DevRathoreSgnr@users.noreply.github.com>
2024-06-25 09:26:46 +0530

    Update connectDB.js

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-    host: '91.108.107.216',
+    // host: '91.108.107.216',
     user: 'u695257503_gateway',
     password: 'eF!1!ylSc@#z',
     database: 'u695257503_gateway'
